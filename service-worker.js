32f7b4436a49808be84696e2e782c58478466a10
shohammallick25 <shohammallick528@gmail.com>
2025-07-16 19:50:34 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,32 @@
-const CACHE_NAME = 'learnsphere-v1';
-const URLS_TO_CACHE = [
-  '/home.html',
-  '/course.html',
-  '/teacher.html',
-  '/price.html',
-  '/review.html',
-  '/contact.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/images/home-img.svg',
-  '/images/12.png'
+const CACHE_NAME = "learnsphere-cache-v1";
+const urlsToCache = [
+  "/LearnSphere/",
+  "/LearnSphere/index.html",
+  "/LearnSphere/contact.html",
+  "/LearnSphere/course.html",
+  "/LearnSphere/price.html",
+  "/LearnSphere/review.html",
+  "/LearnSphere/teacher.html",
+  "/LearnSphere/css/style.css",
+  "/LearnSphere/js/script.js",
+  "/LearnSphere/images/home-img.svg",
+  "/LearnSphere/images/12.png"
 ];
 
-self.addEventListener('install', event => {
+// Install event
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(URLS_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch event
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
